4ccac33e4045cf5212e633945942cd651cb505d7
Shilong Dai <sdai@viperfish.net>
2018-09-29 00:42:55 -0400

    documented api

--- src/main/java/net/viperfish/crawlerApp/core/DependencyType.java
+++ src/main/java/net/viperfish/crawlerApp/core/DependencyType.java
@@ -1,5 +1,8 @@
 package net.viperfish.crawlerApp.core;
 
+/**
+ * Types of dependencies. Each type represents a singular component required by the crawler.
+ */
 public enum DependencyType {
 
 	FETCHER, DATA_SINK, RESTRICTION_MGR
